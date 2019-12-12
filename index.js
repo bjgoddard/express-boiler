f48bc44f79b3006db9e3fd23df4f3f23aa6d29cb
bjgoddard <bjgoddard94@gmail.com>
2019-12-12 14:17:52 -0800

    Layouts

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // Required node modules
 let express = require('express')
+let layouts = require('express-ejs-layouts')
 
 // Declare express app variable
 let app = express()
 
 // Set up and middleware
-
+app.set('view engine', 'ejs')
+app.use(layouts)
 // Add any controllers
 
 // Add home or catch-all routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.render('home')
 })
 
 app.listen(3002, () => {
-    console.log('You are connect on port 3002')
+    console.log('You are connected on port 3002')
 })
