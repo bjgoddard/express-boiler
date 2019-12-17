39638612fe4af2c0fa26ce99da3a0bd6ec368561
bjgoddard <bjgoddard94@gmail.com>
2019-12-17 14:12:42 -0800

    redid error page

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 app.get('*', (req, res) => {
-    res.render('error404')
+    res.render('error')
 })
 
 app.listen(process.env.PORT || 3002, () => {
